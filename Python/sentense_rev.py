2956627640346dae3f5eb5c98a2547ea8ff545e4
Anurag Mittal <anurag.213@gmail.com>
2017-09-06 20:44:14 -0700

    Finally started learning python

---
+++ Python/sentense_rev.py
@@ -0,0 +1,16 @@
+def rev_word(s):
+	words = []
+	length = len(s)
+	space = [' ']
+
+	i = 0
+
+	while i < length:
+		if s[i] not in spaces:
+			word_start = i
+
+			while i < length and s[i] not in spaces:
+				i +=1
+			words.append(s[word_start:i])
+		i += 1
+	return " ".join(reversed(words))
